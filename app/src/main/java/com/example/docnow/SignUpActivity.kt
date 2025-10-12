6b84e5d7ad75f7b555b4f642624293499cf46a7f
FelipeGranziol <felipegranziol@gmail.com>
2025-10-12 20:07:15 -0300

    feat: Telas de login e cadastro (incompletas)

--- app/src/main/java/com/example/docnow/SignUpActivity.kt
+++ app/src/main/java/com/example/docnow/SignUpActivity.kt
@@ -1,15 +1,13 @@
 package com.example.docnow
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,25 +15,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat.startActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.docnow.ui.theme.Azul
 import com.example.docnow.ui.theme.Branco
 import com.example.docnow.ui.theme.DocNowTheme
-import com.example.docnow.ui.theme.Preto
-import com.example.docnow.ui.theme.Vermelho
-import com.example.docnow.ui.theme.Vermelho2
-import com.example.docnow.ui.theme.VermelhoB
-import androidx.compose.ui.platform.LocalContext
 
-class MainActivity : ComponentActivity() {
+class SignUpActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             DocNowTheme {
-
-                    homePage()
+                signUpScreen()
 
             }
         }
@@ -43,31 +35,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun homePage(){
-    val context = LocalContext.current
+fun signUpScreen(){
+    var name by remember { mutableStateOf("") }
+    var birth by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
     Column(
         modifier = Modifier
             .fillMaxSize() ,
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
+        UnderlineTextField(
+            label = "Nome",
+            value = name,
+            isPassword = false,
+            onValueChange = { name = it }
+        )
+        UnderlineTextField(
+            label = "Email",
+            value = email,
+            isPassword = false,
+            onValueChange = { email = it }
+        )
+        UnderlineTextField(
+            label = "Senha",
+            value = password,
+            isPassword = true,
+            onValueChange = { password = it }
+        )
         ButtonDefault(
             color1 = Azul,
             color2 = Branco,
             label = "Cadastrar",
-            onClick = {
-                val intent = Intent(context, SignUpActivity::class.java)
-                context.startActivity(intent)
-            }
-        )
-        ButtonDefault(
-            color1 = Branco,
-            color2 = Azul,
-            label = "Login",
-            onClick = {
-                val intent = Intent(context, LoginActivity::class.java)
-                context.startActivity(intent)
-            }
+            onClick = {""}
         )
     }
 }
